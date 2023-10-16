b3a3f016f266046689a3f3b43ebe10fd0c609e88
DMAND <dmandxist@gmail.com>
2023-10-16 19:36:21 +0300

    Update Form1.cs

--- BrainFEmulator/Form1.cs
+++ BrainFEmulator/Form1.cs
@@ -8,12 +8,6 @@ namespace BrainFEmulator
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if (textBox2.Text.Trim().Length > 0)
